2eb7da621199938f0703780d763a1f14d9eb4b7a
Gregory <gregmax17@gmail.com>
2018-07-13 10:54:11 -0600

    run in the background
    it would lock up for several seconds otherwise

--- src/ios/DiskSpacePlugin.m
+++ src/ios/DiskSpacePlugin.m
@@ -5,28 +5,29 @@
 
 - (void)info:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult = nil;
-    
     long long total = [[[[NSFileManager defaultManager] attributesOfFileSystemForPath:NSHomeDirectory() error:nil] objectForKey:NSFileSystemSize] longLongValue];
     long long free = [[[[NSFileManager defaultManager] attributesOfFileSystemForPath:NSHomeDirectory() error:nil] objectForKey:NSFileSystemFreeSize] longLongValue];
 
-    
-    NSArray *paths = [[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask];
-    NSURL *documentsURL = [paths lastObject];
-    NSUInteger sizeDocuments = [self getDirectoryFileSize:documentsURL];
-    
-    paths = [[NSFileManager defaultManager] URLsForDirectory:NSLibraryDirectory inDomains:NSUserDomainMask];
-    documentsURL = [paths lastObject];
-    NSUInteger sizeLibrairies = [self getDirectoryFileSize:documentsURL];
-    
-    NSMutableDictionary* result = [[NSMutableDictionary alloc] init];
-    [result setObject:[NSNumber numberWithDouble:total] forKey:@"total"];
-    [result setObject:[NSNumber numberWithDouble:(sizeDocuments+sizeLibrairies)] forKey:@"app"];
-    [result setObject:[NSNumber numberWithDouble:free] forKey:@"free"];
-    
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:result];
-    
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    [self.commandDelegate runInBackground:^{
+        NSArray *paths = [[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask];
+        NSURL *documentsURL = [paths lastObject];
+        NSUInteger sizeDocuments = [self getDirectoryFileSize:documentsURL];
+        
+        [self.commandDelegate runInBackground:^{
+            NSArray *paths = [[NSFileManager defaultManager] URLsForDirectory:NSLibraryDirectory inDomains:NSUserDomainMask];
+            NSURL *documentsURL = [paths lastObject];
+            NSUInteger sizeLibrairies = [self getDirectoryFileSize:documentsURL];
+            
+            NSMutableDictionary* result = [[NSMutableDictionary alloc] init];
+            [result setObject:[NSNumber numberWithDouble:total] forKey:@"total"];
+            [result setObject:[NSNumber numberWithDouble:(sizeDocuments+sizeLibrairies)] forKey:@"app"];
+            [result setObject:[NSNumber numberWithDouble:free] forKey:@"free"];
+            
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:result];
+            
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }];
+    }];
 }
 
 -(NSUInteger)getDirectoryFileSize:(NSURL *)directoryUrl
